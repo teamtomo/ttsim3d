b8592394eabdc123873f0a41307953a4e4b44645
Matthew Giammar <43814525+mgiammar@users.noreply.github.com>
2025-06-16 14:05:27 -0700

    feat: add atom batch size parameter to CLI for simulation

--- src/programs/run_ttsim3d.py
+++ src/programs/run_ttsim3d.py
@@ -128,6 +128,16 @@ from ttsim3d.models import Simulator, SimulatorConfig
         "computation only runs on a single GPU. 'cpu' will run on the CPU."
     ),
 )
+@click.option(
+    "--atom-batch-size",
+    type=int,
+    default=16384,
+    help=(
+        "The number of atoms to process (simulate the scattering potentials of) at a "
+        "single time. This is partially controls the memory usage. If -1, the batch "
+        "size calculated automatically. Default is 16384 (2^14)."
+    ),
+)
 def run_simulation_cli(
     pdb_filepath: str,
     mrc_filepath: str,
@@ -145,6 +155,7 @@ def run_simulation_cli(
     apply_dqe: bool,
     mtf_reference: str,
     device: list[int],
+    atom_batch_size: int,
 ) -> None:
     """Run a structure simulation through the CLI."""
     simulator_config = SimulatorConfig(
@@ -157,6 +168,7 @@ def run_simulation_cli(
         apply_dqe=apply_dqe,
         mtf_reference=mtf_reference,
         upsampling=upsampling,
+        atom_batch_size=atom_batch_size,
     )
     simulator = Simulator(
         pixel_spacing=pixel_spacing,
@@ -166,7 +178,7 @@ def run_simulation_cli(
         additional_b_factor=additional_b_factor,
         simulator_config=simulator_config,
     )
-    simulator.export_to_mrc(mrc_filepath=mrc_filepath)
+    simulator.export_to_mrc(device=device, mrc_filepath=mrc_filepath)
 
 
 if __name__ == "__main__":
